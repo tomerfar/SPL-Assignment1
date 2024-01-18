4114d44f70f9a15be40a0fe188172a50a1c67fac
Tomer Faran <tomer.faran@gmail.com>
2024-01-18 16:36:18 +0200

    First Commit

---
+++ src/Order.cpp
@@ -0,0 +1,78 @@
+#include "Order.h"
+#include <iostream> //Check if necessary in this .cpp file
+
+
+//Constructor
+Order::Order(int id, int customerId, int distance):
+ id(id), customerId(customerId), distance(distance),
+ status(OrderStatus::PENDING), collectorId(NO_VOLUNTEER), driverId(NO_VOLUNTEER) {}
+
+ //Methods
+ int Order:: getId() const {
+    return id;
+ }
+ 
+ int Order:: getCustomerId() const{
+    return customerId;
+ }
+
+ void Order:: setStatus(OrderStatus status){
+    this->status = status;
+ }
+
+ void Order:: setCollectorId(int collectorId){
+    this->collectorId = collectorId;
+  }
+
+  void Order:: setDriverId(int driverId){
+    this->driverId = driverId;
+  }
+
+  int Order:: getCollectorId() const{
+    return collectorId;
+  }
+  
+  int Order:: getDriverId() const{
+    return driverId;
+  }
+  OrderStatus Order:: getStatus() const{
+    return status;
+  }
+
+   const std::string statusOrder(OrderStatus status){
+    if (status == OrderStatus::PENDING){
+        return "Pending";
+    }
+    else if (status == OrderStatus::COLLECTING)
+    {
+        return "Collecting";
+    }
+    else if (status == OrderStatus::DELIVERING)
+    {
+        return "Delivering";
+    }
+    else if (status == OrderStatus::COMPLETED)
+    {
+        return "Completed";
+    }
+    return "None";
+  }
+  const std::string Order::toString() const{
+    string str_collector = std::to_string(driverId);
+    if(driverId == NO_VOLUNTEER){
+        str_collector = "None";
+    }
+    string str_driver = std::to_string(collectorId);
+    if(collectorId == NO_VOLUNTEER){
+        str_driver = "None";
+    }
+    
+     return "OrderId " + std::to_string(id) + "\n" + 
+     "OrderStatus " + std::to_string(static_cast<int>(status)) + "\n" + 
+     "CustomerID " + std::to_string(customerId) + "\n" + 
+     "Collector " + str_collector + "\n" + 
+     "Driver " + str_driver;
+  }
+
+
+
